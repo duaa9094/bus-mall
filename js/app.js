5718ef0dcc4121ff9ca0f215e20686b286ad7222
Duaa abudhaim <duaa.abudhaim@gmail.com>
2021-04-19 13:00:59 +0300

    update

--- js/app.js
+++ js/app.js
@@ -143,9 +143,50 @@ function showList(event){
     let li=document.createElement('li');
     ul.appendChild(li);
     li.textContent=products[i].name+" had "+products[i].timesClicked+ " votes, and was seen "+products[i].timesShown+ " times.";
+   
   }
 }
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -174,3 +215,71 @@ function showList(event){
 //   renderThreeProducts();
 //   console.log(products);
 // }
+
+
+
+
+//  \\\\\\\\\\\\\Retadreb
+
+
+// let products=[];
+
+
+
+
+// function Product(name,path){
+//   this.name=name;
+//   this.path=path;
+
+//   this.timesShown=0;
+//   this.timesClicked=0;
+//   products.push(this);
+// }
+
+
+// new Product('bag', 'img/bag.jpg');//[0]
+// new Product('banana', 'img/banana.jpg');//[1]
+// new Product('bathroom', 'img/bathroom.jpg');//[2]
+// new Product('boots', 'img/boots.jpg');
+// new Product('breakfast', 'img/breakfast.jpg');
+// new Product('bubblegum', 'img/bubblegum.jpg');
+// new Product('chair', 'img/chair.jpg');
+// new Product('cthulhu', 'img/cthulhu.jpg');
+// new Product('dog-duck', 'img/dog-duck.jpg');
+// new Product('dragon', 'img/dragon.jpg');
+
+// new Product('pen', 'img/pen.jpg');//[0]
+// new Product('banapet-sweepna', 'img/pet-sweep.jpg');//[1]
+// new Product('scissors', 'img/scissors.jpg');//[2]
+// new Product('shark', 'img/shark.jpg');
+// new Product('sweep', 'img/sweep.png');
+// new Product('tauntaun', 'img/tauntaun.jpg');
+// new Product('unicorn', 'img/unicorn.jpg');
+// new Product('usb', 'img/usb.gif');
+// new Product('dog-water-can', 'img/water-can.jpg');
+// new Product('wine-glass', 'img/wine-glass.jpg');
+
+
+// let firstImage = document.getElementById('firstImage');
+// let secondImage = document.getElementById('secondImage');
+// let thirdImage = document.getElementById('thirdImage');
+
+
+// function genrateRandomIndex() {
+//   return Math.floor(Math.random() * products.length);
+// }
+
+
+// let firstIndex = 0;
+// let secondIndex = 0;
+// let thirdIndex = 0;
+
+
+// function renderThreeProducts()[
+//   firstIndex=genrateRandomIndex;
+//   let firstPath=products[firstIndex].path;
+//   firstImage.setAttribute('src',firstPath);
+
+
+
+// ]
